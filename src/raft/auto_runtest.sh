0748d8a67e7f29f6031133c9f985c39e68bdc0d8
zfhe <2825841950@qq.com>
2024-08-05 22:00:44 +0800

    add common automatic run stress test

--- src/raft/auto_runtest.sh
+++ src/raft/auto_runtest.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-echo "go test -run 3A"
-for i in {1..50}
-do
-  echo "Running test iteration $i"
-  output=$(go test -run 3A 2>&1) # 2>&1 将stderr重定向到stdout
-  if [[ $? -ne 0 ]]; then
-    echo "Error in iteration $i:"
-    echo "$output"
-  fi
-done
+# check the number of arguments
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <test_type> <iterations>"
+  echo "test_type must be one of 3A, 3B, 3C, 3D"
+  exit 1
+fi
 
-echo "go test -run 3B"
-for i in {1..50}
+test_type=$1
+iterations=$2
+
+# check the test_type
+if [[ "$test_type" != "3A" && "$test_type" != "3B" && "$test_type" != "3C" && "$test_type" != "3D" ]]; then
+  echo "Invalid test_type: $test_type"
+  echo "test_type must be one of 3A, 3B, 3C, 3D"
+  exit 1
+fi
+
+# check the iterations is a positive integer
+if ! [[ "$iterations" =~ ^[0-9]+$ ]]; then
+  echo "Invalid iterations: $iterations"
+  echo "iterations must be a positive integer"
+  exit 1
+fi
+
+echo "go test -run $test_type"
+for ((i=1; i<=iterations; i++))
 do
   echo "Running test iteration $i"
-  output=$(go test -run 3B 2>&1) # 2>&1 将stderr重定向到stdout
+  output=$(go test -run $test_type 2>&1) # 2>&1 redirects stderr to stdout
   if [[ $? -ne 0 ]]; then
     echo "Error in iteration $i:"
     echo "$output"
