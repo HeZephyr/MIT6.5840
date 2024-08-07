bcd87ddf305bb541062d685815e17f3973631946
zfhe <2825841950@qq.com>
2024-08-07 22:44:04 +0800

    finish the lab3A

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,6 +1,12 @@
 package raft
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 // Debugging
 const Debug = false
@@ -9,4 +15,92 @@ func DPrintf(format string, a ...interface{}) {
 	if Debug {
 		log.Printf(format, a...)
 	}
+}
+
+const ElectionTimeout = 1000
+const HeartbeatTimeout = 125
+
+type LockedRand struct {
+	mu   sync.Mutex
+	rand *rand.Rand
+}
+
+func (r *LockedRand) Intn(n int) int {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.rand.Intn(n)
+}
+
+var GlobalRand = &LockedRand{
+	rand: rand.New(rand.NewSource(time.Now().UnixNano())),
+}
+
+func RandomElectionTimeout() time.Duration {
+	return time.Duration(ElectionTimeout+GlobalRand.Intn(ElectionTimeout)) * time.Millisecond
+}
+
+func StableHeartbeatTimeout() time.Duration {
+	return time.Duration(HeartbeatTimeout) * time.Millisecond
+}
+
+type ApplyMsg struct {
+	CommandValid bool
+	Command      interface{}
+	CommandIndex int
+
+	// For 3D:
+	SnapshotValid bool
+	Snapshot      []byte
+	SnapshotTerm  int
+	SnapshotIndex int
+}
+
+type NodeState uint8
+
+const (
+	Follower NodeState = iota
+	Candidate
+	Leader
+)
+
+func (state NodeState) String() string {
+	switch state {
+	case Follower:
+		return "Follower"
+	case Candidate:
+		return "Candidate"
+	case Leader:
+		return "Leader"
+	}
+	panic("unexpected NodeState")
+}
+
+func (args RequestVoteArgs) String() string {
+	return fmt.Sprintf("{Term:%v, CandidateId:%v}", args.Term, args.CandidateId)
+}
+
+func (reply RequestVoteReply) String() string {
+	return fmt.Sprintf("{Term:%v, VoteGranted:%v}", reply.Term, reply.VoteGranted)
+}
+
+func (args AppendEntriesArgs) String() string {
+	return fmt.Sprintf("{Term:%v, LeaderId:%v", args.Term, args.LeaderId)
+}
+
+func (reply AppendEntriesReply) String() string {
+	return fmt.Sprintf("{Term:%v, Success:%v}", reply.Term, reply.Success)
+}
+
+type LogEntry struct {
+	Command interface{}
+	Term    int
+	Index   int
+}
+
+func (rf *Raft) getLastLog() LogEntry {
+	return rf.logs[len(rf.logs)-1]
+}
+
+func (rf *Raft) getFirstLog() LogEntry {
+	return rf.logs[0]
 }
